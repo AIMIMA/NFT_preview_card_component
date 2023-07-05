16d5d8715bbaf54d73f67608f7b193b435bb3693
Abdullah Ibrahim Mohammed <ed21.abdullahibrahimm@alexu.edu.ed>
2023-07-06 01:50:56 +0300

    Original Project

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export const content = ["./*.{html,js}"];
 export const darkMode = "class";
 export const theme = {
-  extend: {},
+  extend: {
+    colors: {
+      mainC: "hsl(215, 51%, 70%)",
+      secC: "hsl(178, 100%, 50%)",
+      mainBg: "hsl(217, 54%, 11%)",
+      cardBg: "hsl(216, 50%, 16%)",
+      line: "hsl(215, 32%, 27%)",
+    },
+  },
 };
 export const plugins = [];
